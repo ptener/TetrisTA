96438b181599002a914c4fd922faa5a1da91d406
Patrick Tener <ptener@umail.iu.edu>
2021-01-04 01:28:13 -0500

    Moving blocks with combo of physics and hard code.  Still problems with using physics

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    // List of avaliable blocks
+    public List<GameObject> blocks;
+
+    public bool blockPlaced = true;
+
+    // Vars to keep track of the current and next block
+    public GameObject currentBlock;
+    public GameObject nextBlock;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set a starting difficulty
+        nextBlock = blocks[Random.Range(0, blocks.Count)];
+        currentBlock = blocks[Random.Range(0, blocks.Count)];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (blockPlaced)
+        {
+            blockPlaced = false;
+
+            currentBlock = nextBlock;
+
+            nextBlock = blocks[Random.Range(0, blocks.Count)];
+
+            Instantiate(currentBlock, transform);
+
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+
+    public bool currentBlock = true;
+    public BlockSpawner blockspawner;
+
+    private float currentDelay;
+    private float totalDelay = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blockspawner = GameObject.Find("BlockSpawner").GetComponent<BlockSpawner>();
+        currentDelay = totalDelay;
+    }
+
+    private void Update()
+    {
+        if (currentBlock)
+        {
+            // Right and left
+            if (Input.GetButtonDown("Horizontal"))
+            {
+                if (Input.GetAxis("Horizontal") > 0)
+                {
+                    transform.position += new Vector3(1, 0, 0);
+                    currentDelay = totalDelay;
+                }
+                if (Input.GetAxis("Horizontal") < 0)
+                {
+                    transform.position += new Vector3(-1, 0, 0);
+                    currentDelay = totalDelay;
+                }
+            }
+            // Up and down
+            if (Input.GetButtonDown("Vertical"))
+            {
+                if (Input.GetAxis("Vertical") > 0)
+                {
+                    // Rotate
+                }
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    transform.position += new Vector3(0, -1, 0);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        currentDelay = totalDelay;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (currentBlock)
+        {
+
+            currentDelay -= Time.deltaTime;
+
+            if (currentDelay < 0)
+            {
+                if (!blockspawner.blockPlaced)
+                {
+                    transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), Mathf.Round(transform.position.z));
+                    blockspawner.blockPlaced = true;
+                    currentBlock = false;
+                }
+            }
+            
+        }
+        
+    }
+
+}
